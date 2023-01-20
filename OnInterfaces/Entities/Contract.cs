ca104c0d4561ea8b35a6948c23ed52ae1934f901
Wesley <wesley1471@gmail.com>
2023-01-20 12:48:16 -0300

    added entities, services

---
+++ OnInterfaces/Entities/Contract.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnInterfaces.Entities
+{
+    internal class Contract
+    {
+        public int Number { get; set; }
+        public DateTime Date { get; set; }
+        public double TotalValue { get; set; }
+
+        public Contract(int number, DateTime date, double totalValue)
+        {
+            Number = number;
+            Date = date;
+            TotalValue = totalValue;
+        }
+    }
+}
